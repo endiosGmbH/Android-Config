8eb1c837f55d900122af66932d414d118396b401
Kanishk Verma <kanishk.verma@endios.de>
2021-05-19 15:16:09 +0200

    OP-2311 - Update pre-commit GIT hook and relevant tooling setup
    - update function names used in messages to be more explicit

--- tooling/install-tooling.sh
+++ tooling/install-tooling.sh
@@ -12,7 +12,7 @@ echo "Copying GIT hook..."
 cp -fr ${SCRIPTDIR}/hooks ${PROJECTDIR}/.git
 
 # ktlint
-echo "Downloading & setting up ktlint binary...(Machine Password may be asked once if ktlint binary is not configured)"
+echo "Downloading & setting up ktlint binary...(Machine's User Password may be asked once if ktlint binary is not configured)"
 # downloading the ktlint binary, marking it as executable and then moving it into the required directory
 curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.40.0/ktlint &&
   chmod a+x ktlint &&
@@ -21,7 +21,7 @@ curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.40.0/ktlint &
 # another option is "brew install ktlint". Refer: https://github.com/pinterest/ktlint#installation
 
 # detekt
-echo "Downloading & setting up detekt binary...(Machine Password may be asked once if detekt binary is not configured)"
+echo "Downloading & setting up detekt binary...(Machine's User Password may be asked once if detekt binary is not configured)"
 # downloading the detekt archive and unzipping into the required directory
 curl -sSLO https://github.com/detekt/detekt/releases/download/v1.17.0/detekt-cli-1.17.0.zip &&
   unzip -o detekt-cli-1.17.0.zip -d /usr/local/bin/
